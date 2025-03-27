25905900212fd64ea4b7c07c001ac2416677e94f
Jhon Briones <joanbric@gmail.com>
2025-03-27 17:54:01 +0000

    Just formatting

--- app/sketch/view/[id]/page.tsx
+++ app/sketch/view/[id]/page.tsx
@@ -5,8 +5,6 @@ import { useParams } from 'next/navigation'
 const Canvas = dynamic(() => import('@components/Canvas'), { ssr: false })
 export default function Page() {
   const { id } = useParams()
-  
-
   return (
     <>
       <Canvas sketchID={id as string} showPath />  
