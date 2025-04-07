f57a6193c315632d40c9d3ecac9ad424b2998038
Jhon Briones <joanbric@gmail.com>
2025-04-07 20:02:36 +0100

    Temporary OG and configs for the theme managment

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,9 @@ import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
 import { ReactNode } from 'react'
 import Header from '@components/Header'
+import { ThemeProvider } from './context/ThemeContext'
+import { cookies } from 'next/headers'
+
 const geistSans = Geist({
   variable: '--font-geist-sans',
   subsets: ['latin']
@@ -16,7 +19,36 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: 'SuperPathMarket',
   description:
-    'Shop smarter & save time! Our app maps your supermarket list into the fastest route, guiding you straight to each item. Never wander aisles again—check off your list in record time!'
+    'Shop smarter & save time! Our app maps your supermarket list into the fastest route, guiding you straight to each item. Never wander aisles again—check off your list in record time!',
+  openGraph: {
+    title: 'SuperPathMarket',
+    description:
+      'Shop smarter & save time! Our app maps your supermarket list into the fastest route, guiding you straight to each item. Never wander aisles again—check off your list in record time!',
+    type: 'website',
+    url: 'https://superpathmarket.com',
+    siteName: 'SuperPathMarket',
+    images: [
+      {
+        url: 'https://superpathmarket.com/og-image.png',
+        width: 1200,
+        height: 630
+      }
+    ]
+  },
+  twitter: {
+    title: 'SuperPathMarket',
+    description:
+      'Shop smarter & save time! Our app maps your supermarket list into the fastest route, guiding you straight to each item. Never wander aisles again—check off your list in record time!',
+    card: 'summary_large_image',
+    site: 'https://superpathmarket.com',
+    images: [
+      {
+        url: 'https://superpathmarket.com/og-image.png',
+        width: 1200,
+        height: 630
+      }
+    ]
+  }
 }
 
 export default async function RootLayout({
@@ -24,15 +56,27 @@ export default async function RootLayout({
 }: Readonly<{
   children: ReactNode
 }>) {
+  const cookieStore = await cookies()
+  const themeCookie = cookieStore.get('theme')
+  const savedTheme = themeCookie?.value as 'light' | 'dark' | undefined
+  const theme = savedTheme || 'light'
+
   return (
-    <html lang="en">
+    <html
+      lang="en"
+      className={theme}
+    >
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased dark:bg-gray-900`}
         style={{}}
         draggable={false}
       >
-        <Header />
-        {children}
+        <ThemeProvider>
+          <Header />
+          {/* <div className="bg-gradient-to-b dark:from-gray-900 dark:to-gray-800 from-blue-50 min-h-screen to-white transition-colors"> */}
+          {children}
+          {/* </div> */}
+        </ThemeProvider>
       </body>
     </html>
   )
