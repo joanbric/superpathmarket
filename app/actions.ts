b733635096003a57a4e5d5e48d49662b3c546065
Jhon Briones <joanbric@gmail.com>
2025-04-09 19:54:51 +0100

    Refactor product creation logic and enhance EditProductModal with store price management

--- app/actions.ts
+++ app/actions.ts
@@ -1,5 +1,5 @@
 'use server'
-
+import { z } from 'zod'
 import { saveFile } from '@libs/R2Manager'
 import { createClient } from '@libsql/client'
 
@@ -8,23 +8,51 @@ const turso = createClient({
   authToken: process.env.TURSO_AUTH_TOKEN!
 })
 
-export async function createProduct(form: FormData) {
-  console.log('El formulario del action', form)
-  const file: File = form.get('image') as File
-  const name = form.get('name') as string
+const productSchema = z.object({
+  name: z.string().nonempty(),
+  plu: z.string(),
+  image: z.instanceof(File).optional(),
+  prices: z
+    .array(
+      z.object({
+        storeId: z.number().nonnegative().int(),
+        price: z.number().nonnegative().int()
+      })
+    )
+    .nonempty()
+})
 
+export async function createProduct(form: FormData) {
+  const { name, plu, image } = Object.fromEntries(form.entries())
+  const prices = Array.from(form.entries())
+    .filter(([name]) => name === 'store' || name === 'price')
+    .reduce((acc, [name, value]) => {
+      if (name === 'store') acc.push({ storeId: Number(value), price: -1 })
+      if (name === 'price') acc[acc.length - 1].price = Number(value)
+      return acc
+    }, [] as { storeId: number; price: number }[])
+
+  const productValidation = productSchema.safeParse({ name, plu, image, prices })
+  if (!productValidation.success) throw new Error(productValidation.error.message)
   try {
-    const url = await saveFile(file)
-    // console.log(url)
-    await turso.execute('INSERT INTO products (name, img_url, price ) VALUES (?, ?, ?)', [name, url, 0])
+    const product = productValidation.data
+    let url = null
+    if (product.image && product.image.size > 0) {
+      url = await saveFile(product.image)
+    }
+
+    const { lastInsertRowid } = await turso.execute('INSERT INTO products (name, img_url) VALUES (?, ?)', [product.name, url ? url : null])
+    if (!lastInsertRowid) throw new Error('Failed to create product')
+    const sqlValues = product.prices.map(() => '(?, ?, ?, ?)').join(', ')
+    const sqlParams = product.prices.flatMap(p => [Number(lastInsertRowid), p.storeId, p.price, Date.now()])
+    const query = `INSERT INTO product_prices (product_id, store_id, price, date_created) VALUES ${sqlValues}`
+    await turso.execute(query, sqlParams)
   } catch (error) {
     console.log(error)
   }
 }
 
-
 export async function createStore(form: FormData) {
-  console.log('El formulario del action', form)
   const name = form.get('name') as string
   const address = form.get('address') as string
 
--- app/app/products/EditProductModal.tsx
+++ app/app/products/EditProductModal.tsx
@@ -1,17 +1,34 @@
 'use client'
-import { Button, FileInput, Label, ModalFooter, TextInput } from 'flowbite-react'
-import ModalComponent from '../../components/Modal'
+import { Button, Card, FileInput, Label, ListGroup, ModalFooter, Select, TextInput } from 'flowbite-react'
+import ModalComponent from '@components/Modal'
 import { createProduct } from '@/actions'
-import { useState } from 'react'
-import { PlusIcon, SaveIcon, XCircle } from 'lucide-react'
+import { useEffect, useState } from 'react'
+import { PlusIcon, SaveIcon, Trash2, XCircle } from 'lucide-react'
+import { useRouter } from 'next/navigation'
+import { Store } from '@/types'
 
-export default function EditProductModal({
-  operationType
-}: {
-  operationType: 'create' | 'edit'
-}) {
+type StorePrice = {
+  storeId: number
+  price: number
+}
+
+export default function EditProductModal({ operationType }: { operationType: 'create' | 'edit' }) {
   const [imagePreview, setImagePreview] = useState<string>('')
   const [showModal, setShowModal] = useState(false)
+  const router = useRouter()
+  const [stores, setStores] = useState<Store[]>([])
+  const [storePrices, setStorePrices] = useState<StorePrice[]>([{ storeId: 0, price: 0 }])
+
+  const getRemainingStores = (storeId: number) =>
+    stores.filter(store => store.id === storeId || !storePrices.some(storePrice => storePrice.storeId === store.id))
+
+  useEffect(() => {
+    fetch('/api/stores')
+      .then(res => res.json())
+      .then(data => {
+        setStores(data)
+      })
+  }, [])
 
   if (!showModal && imagePreview !== '') {
     setImagePreview('')
@@ -19,6 +36,22 @@ export default function EditProductModal({
 
   const closeModal = () => {
     setShowModal(false)
+  }
+
+  const handleSubmit = () => {
+    router.replace('/app/products')
+    closeModal()
+  }
+  const handleAddStorePrice = () => {
+    setStorePrices([...storePrices, { storeId: 0, price: 0 }])
+  }
+  const handleDeleteStorePrice = (storeId: number) => {
+    const newStorePrices = [...storePrices]
+    newStorePrices.splice(
+      newStorePrices.findIndex(storePrice => storePrice.storeId === storeId),
+      1
+    )
+    setStorePrices(newStorePrices)
   }
 
   return (
@@ -35,14 +68,21 @@ export default function EditProductModal({
         show={showModal}
         titleHeader={operationType === 'create' ? 'New Product' : 'Edit [product name]'}
         closeModal={closeModal}
-        onSubmit={() => {}}
+        onSubmit={handleSubmit}
+        theme={{
+          body: { base: 'px-1 md:px-6' },
+          content: { base: 'px-2 md:px-4' },
+          header: { base: 'px-2 md:px-5' },
+          footer: { base: 'px-2 md:px-5' }
+        }}
       >
         <form
           action={createProduct}
           onSubmit={closeModal}
           className="space-y-4"
         >
-          <div className="flex items-center justify-center my-3 w-full">
+          <Card theme={{ root: { children: 'p-4' } }}>
+            {/* <div className="flex items-center justify-center my-3 w-full"> */}
             <Label
               htmlFor="dropzone-file"
               className="w-full"
@@ -86,15 +126,102 @@ export default function EditProductModal({
                 }}
               />
             </Label>
-          </div>
-          <Label htmlFor="name">Name</Label>
-          <TextInput
-            id="name"
-            placeholder="Sugar"
-            name="name"
-            required
-          />
-          <ModalFooter>
+            {/* </div> */}
+          </Card>
+          <Card theme={{ root: { children: 'p-4' } }}>
+            <Label htmlFor="name">Name</Label>
+            <TextInput
+              id="name"
+              placeholder="Sugar"
+              name="name"
+              required
+            />
+            <Label htmlFor="plu">PLU</Label>
+            <TextInput
+              id="plu"
+              placeholder="123456789"
+              name="plu"
+              required
+            />
+          </Card>
+
+          <Card
+            className="w-full"
+            theme={{ root: { children: 'p-4' } }}
+          >
+            <header className="flex flex-wrap items-center justify-between w-full">
+              <h5 className="dark:text-white font-semibold text-gray-900 text-lg tracking-tight">Store Prices</h5>
+              <Button
+                className="flex font-bold gap-2 items-center"
+                content="hola"
+                size="sm"
+                outline
+                pill
+                theme={{ outlineColor: { default: 'border md:border-0' }, size: { sm: 'px-0 h-fit md:h-9 md:px-3' } }}
+                onClick={handleAddStorePrice}
+              >
+                <PlusIcon /> <span className="hidden md:inline">Add Store Price</span>
+              </Button>
+            </header>
+
+            <ListGroup>
+              {storePrices.map((storePrice, index) => (
+                <li
+                  key={index}
+                  className="border-b border-gray-200 dark:border-gray-600 gap-2 grid grid-cols-[6fr_1fr] grid-flow-col grid-rows-2 px-2 py-2 w-full md:flex md:gap-2 md:items-center"
+                >
+                  <div className="flex flex-col gap-2 md:flex-2">
+                    <Label htmlFor="store">Store</Label>
+                    <Select
+                      id="store"
+                      name="store"
+                      value={storePrice.storeId}
+                      className="is_active"
+                      onChange={e => {
+                        const newStorePrices = [...storePrices]
+                        newStorePrices[index].storeId = Number(e.target.value)
+                        setStorePrices(newStorePrices)
+                      }}
+                    >
+                      <option value="0">Select Store</option>
+                      {getRemainingStores(storePrice.storeId).map(store => (
+                        <option
+                          key={store.id}
+                          value={store.id}
+                        >
+                          {store.name} --&gt; {store.address.split(',')[0]}
+                        </option>
+                      ))}
+                    </Select>
+                  </div>
+                  <div className="flex flex-col gap-2 md:flex-1">
+                    <Label htmlFor="price">Price</Label>
+                    <TextInput
+                      id="price"
+                      placeholder="599"
+                      name="price"
+                      required
+                      value={storePrice.price}
+                      onChange={e => {
+                        const newStorePrices = [...storePrices]
+                        newStorePrices[index].price = Number(e.target.value)
+                        setStorePrices(newStorePrices)
+                      }}
+                    />
+                  </div>
+                  <button
+                    onClick={() => handleDeleteStorePrice(storePrice.storeId)}
+                    className="active:bg-red-500 active:dark:bg-red-700 active:text-white border-0 dark:text-white flex h-full hover:bg-red-500 hover:dark:bg-red-700 hover:text-white items-center justify-center outline-0 p-1 place-self-center rounded-lg row-span-2 text-red-500 w-full md:flex-0 md:place-self-end md:py-2"
+                    type="button"
+                  >
+                    <Trash2 />
+                  </button>
+                </li>
+              ))}
+            </ListGroup>
+          </Card>
+
+          <ModalFooter className="justify-end">
             <Button
               color="red"
               onClick={closeModal}
