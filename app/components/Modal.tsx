538f9f64ca88b7e1dcdba64ad8237f0df07a318e
Jhon Briones <joanbric@gmail.com>
2025-04-09 19:53:42 +0100

    Refactor ModalComponent to use a dedicated interface for props

--- app/components/Modal.tsx
+++ app/components/Modal.tsx
@@ -1,23 +1,26 @@
-import { Modal, ModalBody, ModalHeader } from 'flowbite-react'
+import { Modal, ModalBody, ModalHeader, ModalProps } from 'flowbite-react'
+interface ModalComponentProps extends ModalProps {
+  titleHeader: string
+  children: React.ReactNode
+  show: boolean
+  closeModal: () => void
+  onSubmit: () => void
+}
 
 export default function ModalComponent({
   titleHeader,
   children,
   show,
   closeModal,
-  onSubmit
-}: {
-  titleHeader: string
-  children: React.ReactNode
-  show: boolean
-  closeModal: () => void
-  onSubmit: () => void
-}) {
+  onSubmit,
+  ...props
+}: ModalComponentProps) {
   return (
     <Modal
       show={show}
       onClose={closeModal}
       onSubmit={onSubmit}
+      {...props}
     >
       <ModalHeader>{titleHeader}</ModalHeader>
       <ModalBody>{children}</ModalBody>
