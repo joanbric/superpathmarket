0ad8eb10c885e8186d6e6835ccbbeedc72f7fbb5
Jhon Briones <joanbric@gmail.com>
2025-04-18 01:00:07 +0100

    Remove experimental webpack configuration and related options from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -7,18 +7,7 @@ const nextConfig: NextConfig = {
     // This enables the new JSX transform from React 17
     reactRemoveProperties: process.env.NODE_ENV === 'production',
     removeConsole: process.env.NODE_ENV === 'production'
-  },
-  experimental: {
-    turbo: {
-      resolveAlias: {
-        canvas: { browser: './empty.js' }
-      }
-    }
-  },
-  webpack: config => {
-    config.externals = [...config.externals, { canvas: 'canvas' }]
-    return config
-  }
+  }  
 }
 
 export default withFlowbiteReact(nextConfig)
